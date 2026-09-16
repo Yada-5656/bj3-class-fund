9aa376dd27317f10956b7bfbd2c14cfd7bf3c6d0
BJ3 Admin <bj3@example.com>
2026-09-16 18:06:44 +0700

    chore: clean up test files

--- test.js
+++
@@ -1,2 +0,0 @@
-const STORE_URL = 'https://api.restful-api.dev/objects/ff808181a09d98f701a0a664936f137c';
-fetch(STORE_URL).then(res => res.json()).then(data => console.log(JSON.stringify(data, null, 2)));
